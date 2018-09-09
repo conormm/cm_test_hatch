f399e28deac61cbc2514af2f7345e8f79e6be772
Conor McDonald <conrmcdonald@gmail.com>
2018-09-09 20:04:53 +0100

    updating

--- cm_test_hatch/hellot_world.py
+++ cm_test_hatch/hellot_world.py
@@ -1,2 +1,10 @@
-def hello():
-    print("hello world")
+import fire
+
+class Calculator(object):
+  """A simple calculator class."""
+
+  def double(self, number):
+    return 2 * number
+
+if __name__ == '__main__':
+  fire.Fire(Calculator)
